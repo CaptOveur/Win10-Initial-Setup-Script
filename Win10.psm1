0e892979386bf8195f83767cdb4b1594b06c6e06
CaptOveur <ryanmccullough09@gmail.com>
2019-02-18 14:22:12 -0800

    Update Win10.psm1
    Modified standby-timeout-ac from 30 to 0

--- Win10.psm1
+++ Win10.psm1
@@ -1208,7 +1208,7 @@ Function EnableSleepTimeout {
 	Write-Output "Enabling display and sleep mode timeouts..."
 	powercfg /X monitor-timeout-ac 10
 	powercfg /X monitor-timeout-dc 5
-	powercfg /X standby-timeout-ac 30
+	powercfg /X standby-timeout-ac 0
 	powercfg /X standby-timeout-dc 15
 }
 
